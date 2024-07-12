f216ea90f696bc2cf6740d8a49651927b5ee55b4
AnnaMaxion <anna.maxion@rwth-aachen.de>
2024-07-12 10:27:36 +0200

    remove test files

--- Untitled7.m
+++
@@ -1,5 +0,0 @@
-folder = strcat("D:\",'EDFData\', 'Risperidone', '\CleanData');
-
-listEDF = d.createFileList('edf',folder);
-
-EEGData = pop_biosig(listEDF{0},'importevent','off','importannot','off'); 
